8ca9c42ff3d66fe3b0485ace0cc3969864106f75
kasperhhk <kkr@dis-play.dk>
2020-04-04 14:24:52 +0200

    add new core lib

---
+++ dist/core/esm5/lib/core.service.js
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import * as i0 from "@angular/core";
+var CoreService = /** @class */ (function () {
+    function CoreService() {
+    }
+    CoreService.ɵfac = function CoreService_Factory(t) { return new (t || CoreService)(); };
+    CoreService.ɵprov = i0.ɵɵdefineInjectable({ token: CoreService, factory: CoreService.ɵfac, providedIn: 'root' });
+    return CoreService;
+}());
+export { CoreService };
+/*@__PURE__*/ (function () { i0.ɵsetClassMetadata(CoreService, [{
+        type: Injectable,
+        args: [{
+                providedIn: 'root'
+            }]
+    }], function () { return []; }, null); })();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29yZS5zZXJ2aWNlLmpzIiwic291cmNlUm9vdCI6Im5nOi8vY29yZS8iLCJzb3VyY2VzIjpbImxpYi9jb3JlLnNlcnZpY2UudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxFQUFFLFVBQVUsRUFBRSxNQUFNLGVBQWUsQ0FBQzs7QUFFM0M7SUFLRTtJQUFnQixDQUFDOzBFQUZOLFdBQVc7dURBQVgsV0FBVyxXQUFYLFdBQVcsbUJBRlYsTUFBTTtzQkFIcEI7Q0FRQyxBQU5ELElBTUM7U0FIWSxXQUFXO2tEQUFYLFdBQVc7Y0FIdkIsVUFBVTtlQUFDO2dCQUNWLFVBQVUsRUFBRSxNQUFNO2FBQ25CIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgSW5qZWN0YWJsZSB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuXG5ASW5qZWN0YWJsZSh7XG4gIHByb3ZpZGVkSW46ICdyb290J1xufSlcbmV4cG9ydCBjbGFzcyBDb3JlU2VydmljZSB7XG5cbiAgY29uc3RydWN0b3IoKSB7IH1cbn1cbiJdfQ==
